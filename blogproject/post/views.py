c9067367d54d165bdce4b9d4bbd0eec6080c9aff
Praseesh <praseeshprasee@gmail.com>
2024-09-14 10:02:48 +0530

    removed old PostView

--- blogproject/post/views.py
+++ blogproject/post/views.py
@@ -12,17 +12,7 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
 from rest_framework.exceptions import AuthenticationFailed
 
-# class PostView(APIView):
-#     authentication_classes = [JWTAuthentication]  
-#     permission_classes = [IsAuthenticated]
-#     def get(self,request,*args, **kwargs):
-#         if not request.user.is_authenticated:
-#             return Response({'error':'Authentication Credentials are not provided'},
-#                             status=status.HTTP_401_UNAUTHORIZED)
-#         else:
-#             posts = Posts.objects.all()
-#             serializer = PostViewSerializer(posts,many=True)
-#             return Response (serializer.data,status=status.HTTP_200_OK)
+
 class PostView(APIView):
     authentication_classes = [JWTAuthentication]  
     permission_classes = [IsAuthenticated]
